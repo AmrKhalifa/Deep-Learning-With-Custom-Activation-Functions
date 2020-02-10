a37ec51a136da57abe4d52d4453444ddc81e7f7d
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2020-02-10 16:42:10 +0200

    added code for mandelbrot activation

---
+++ activations.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class MandelbrotActivation(nn.Module): 
+
+	def __init__(self, lmbda = None):
+		super(MandelbrotActivation, self).__init__()
+		#self.in_features = in_features
+
+		if lmbda == None:
+			self.lmbda = torch.nn.Parameter(torch.tensor(0.0))
+		else:
+			self.lmbda = torch.nn.Parameter(torch.tensor(lmbda))
+
+		self.lmbda.requires_grad = True
+
+	def forward(self, x):
+
+		return self.lmbda*(x)*(1-x)
+
