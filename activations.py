f2d36ef0557374a3054cdb851e61236d65c957f1
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2020-02-18 19:04:45 +0200

    modified functions

--- activations.py
+++ activations.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 class MandelbrotActivation(nn.Module): 
 
-	def __init__(self, tensor):
+	def __init__(self, shapes):
 		super(MandelbrotActivation, self).__init__()
 
-		self.lmbda = torch.nn.Parameter(torch.randn(tensor.shape))
+		self.lmbda = torch.nn.Parameter(torch.randn(shapes)*.25 + .5)
 		self.lmbda.requires_grad = True
 
 	def forward(self, x):
